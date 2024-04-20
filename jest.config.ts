d6d7270beed335c9a129dac181c05ad758fdee99
NhatHuynh <nhatthuynhh621@gmail.com>
2024-04-20 22:25:01 +0700

    chore: setup nx, husky, editorconfig, lint-staged, commitlint

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,8 @@ export default {
   preset: './jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: './coverage/nest-monorepo-skeleton',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
 };
